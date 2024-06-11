5cc244fdb0e460690de7d69e33ad855eb3d9a6aa
prasadp1307 <prasaddp1137@gmail.com>
2024-06-11 15:10:56 +0530

    add user to database

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const app = express();
+
+const sequelize = require('./database/db');
+const Users = require('./util/user')
+
 
 app.use(cors());
 app.use(express.json());
@@ -13,10 +18,23 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'signUp.html'));
 });
 
-app.post('/', (req, res) => {
-    res.send('Form submitted successfully');
+app.post('/', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        await Users.create({ name, email, password });
+        res.send('Form submitted successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error submitting form');
+    }
 });
 
-app.listen(4000, () => {
-    console.log("Server running successfully on port 4000");
-});
+sequelize.sync()
+.then((res)=>{
+    app.listen(4000);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
